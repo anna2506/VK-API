b56d463f42746444dc9740679c1b3015262f336e
Anna Doshchenko <annadoshenko3@gmail.com>
2020-04-14 11:45:52 +0300

    Removed unnesessary staff

--- app.js
+++ app.js
@@ -15,29 +15,16 @@ else{
 
 
 document.getElementById('vkLogin').onclick = function(){
-    console.log("login");
     login();
-
-    /*const p = new Promise((resolve) => {
-        login();
-        resolve();
-    });
-    p.then(() => getFriends());
-    let promise = new Promise(login);
-    promise.then(getFriends());*/
-    console.log("friends");
-}
+};
 
 function login(){
     VK.Auth.login(function (response) {
-        console.log(response);
         if(response.status === 'connected'){
             localStorage.id = response.session.user.id;
-            console.log('yes');
+            console.log('connected');
             document.getElementById('vkLogin').hidden = true;
             document.getElementById('vkLogout').hidden = false;
-            console.log("debug");
-            console.log(response.session.user);
             /* сохраняем имя и фамилию пользователя*/
             localStorage.name = response.session.user.first_name;
             localStorage.surname = response.session.user.last_name;
@@ -50,15 +37,12 @@ function login(){
         else if(response.status === 'unknown '){
             console.log("You need to register in Vkontakte");
         }
-
-
     }, VK.access.FRIENDS);
 }
 
 
 function name() {
     if(localStorage.name && localStorage.surname){
-        console.log("name");
         var hi = document.getElementById('hello');
         var hey = document.createElement('span');
         hey.style.display = "flex";
@@ -78,7 +62,6 @@ function name() {
 }
 function getFriends() {
     if (localStorage.name && localStorage.surname) {
-        console.log("getFriends function");
         VK.Api.call('friends.get',
             {
                 user_id: localStorage.id,
@@ -87,7 +70,6 @@ function getFriends() {
                 v: "5.73"
             },
             function (r) {
-                console.log(r);
                 var div = document.getElementById('friends');
                 var ul = document.getElementById('friendsList');
                 var li1 = document.createElement('li');
@@ -107,14 +89,7 @@ function getFriends() {
                 var arr = [];
                 for (let i = 0; i < r.response.items.length; i++) {
                     friend = r.response.items[i];
-                    console.log('friend');
-                    /*list = friend.first_name + " " + friend.last_name;
-                    console.log(list);
-                    arr.push(list);*/
                     VK.Api.call('users.get', {user_ids: friend, v:"5.73"}, function(resp){
-                        console.log('name');
-                        console.log(resp);
-                        console.log(resp.response[0].first_name);
                         let list = resp.response[0].first_name + " " + resp.response[0].last_name;
                         var li = document.createElement('li');
                         li.innerHTML = list;
@@ -125,32 +100,9 @@ function getFriends() {
                             ul.appendChild(li);
                             arr.push(list);
                         }
-                        console.log(list);
-                        console.log(arr);
                         localStorage.setItem("friends", JSON.stringify(arr));
-                        console.log(localStorage.getItem("friends"));
                     });
-                    console.log(arr);
-                    localStorage.setItem("friends", JSON.stringify(arr));
-                    console.log(localStorage.getItem("friends"));
                 }
-
-                for (let i = 0; i < arr.length; i++) {
-                    console.log("array: ", 1);
-                    console.log(arr[i]);
-                    var li = document.createElement('li');
-                    li.innerHTML = arr[i];
-                    li.style.textAlign = "center";
-                    li.style.fontWeight = "bold";
-                    li.style.fontSize = "16pt";
-                    if(li){
-                        ul.appendChild(li);
-                    }
-
-
-                }
-                div.appendChild(ul);
-                console.log('friends');
             });
     }
 }
@@ -164,7 +116,6 @@ onPageLoad();
 function onLoadFriends(){
     if(localStorage.getItem("friends")){
         var array = JSON.parse(localStorage.getItem("friends"));
-        console.log(array);
         var div = document.getElementById('friends');
         var ul = document.getElementById('friendsList');
         var li1 = document.createElement('li');
@@ -190,7 +141,6 @@ function onLoadFriends(){
             ul.appendChild(li);
         }
         div.appendChild(ul);
-        console.log("same friends");
     }
     else{
         getFriends();
@@ -198,12 +148,9 @@ function onLoadFriends(){
 }
 
 document.getElementById('vkLogout').onclick = function(){
-    console.log("logout");
     VK.Auth.logout(function(response){
-        console.log(response);
         document.getElementById('vkLogin').hidden = false;
         document.getElementById('vkLogout').hidden = true;
-        //document.getElementById('friendsList').hidden = true;
         var hi = document.getElementById('hello');
         while (hi.firstElementChild){
             hi.removeChild(hi.firstElementChild);
@@ -221,33 +168,3 @@ document.getElementById('vkLogout').onclick = function(){
     });
 
 };
-
-/*document.getElementById('vkFriends').onclick = function () {
-    console.log('friends');
-    var ul = document.getElementById('friendsList');
-    var li = document.createElement('li');
-    li.style.textAlign = "center";
-    li.style.fontWeight = "bold";
-    li.style.fontSize = "20pt";
-    li.innerHTML = "Ваши друзья";
-    if(ul.childElementCount != 0){
-        ul.removeChild(ul.firstElementChild);
-    }
-
-    ul.appendChild(li);
-    document.getElementById('friendsList').hidden = false;
-    VK.Api.call(
-        'friends.get',{
-            user_id: localStorage.id,
-            count: 5,
-
-        }, function (response){
-            let list = document.createElement("li");
-            for(let i = 0; i < response.response.items.length; i++){
-                let friend = response.response.items[i];
-                list.appendChild(friend.first_name + " " + friend.last_name);
-            }
-            list.innerHTML(list);
-    })
-};*/
-
